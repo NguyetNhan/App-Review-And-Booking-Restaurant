e6eff5bc3cca86f2e0c15897b802a4ea2a88d2aa
NguyetNhan <43900183+NguyetNhan@users.noreply.github.com>
2019-08-06 10:48:22 +0700

    update

--- app/src/admin_restaurant/register_restaurant/components/index.js
+++ app/src/admin_restaurant/register_restaurant/components/index.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, TextInput, } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, TextInput, PermissionsAndroid, Image, Modal, FlatList, Dimensions } from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
 import Icon from 'react-native-vector-icons/AntDesign';
+
 export default class RegisterRestaurant extends Component {
 
         constructor (props) {
                 super(props);
+                this.state = {
+                        modalVisible: false,
+                        data: [0, 1, 2, 6, 8, 16, 46, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]
+                };
+                this.requestCameraPermission();
         }
+
+        async  requestCameraPermission () {
+                try {
+                        const granted = await PermissionsAndroid.request(
+                                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+                        );
+                        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                                console.log('You can use the data');
+                        } else {
+                                console.log('Data permission denied');
+                        }
+                } catch (err) {
+                        console.warn(err);
+                }
+        }
+
+        async   _handleButtonPress () {
+                var result = await CameraRoll.getPhotos({
+                        first: 20,
+                        assetType: 'Photos',
+                });
+                console.log('result: ', result);
+        }
+
+
+
         render () {
+                const { height, width } = Dimensions.get('window');
                 return (
                         <View style={styles.container}>
                                 <ScrollView  >
@@ -48,12 +82,74 @@ export default class RegisterRestaurant extends Component {
                                                         marginTop: 10,
                                                         flex: 1
                                                 }}>Ảnh giới thiệu</Text>
-                                                <TouchableOpacity>
+                                                <TouchableOpacity onPress={() => {
+                                                        this.setState({
+                                                                modalVisible: !this.state.modalVisible
+                                                        });
+                                                        this._handleButtonPress();
+                                                }} >
                                                         <Icon name='picture' size={25} color='black' />
                                                 </TouchableOpacity>
                                         </View>
-
                                 </ScrollView>
+                                <Modal
+                                        animationType="slide"
+                                        transparent={false}
+                                        visible={this.state.modalVisible} >
+                                        <View style={styles.containerModal}>
+                                                <View style={styles.headerModal}>
+                                                        <TouchableOpacity onPress={() => {
+                                                                this.setState({
+                                                                        modalVisible: !this.state.modalVisible
+                                                                });
+                                                        }} >
+                                                                <Icon name='down' size={30} color='#22D499' />
+                                                        </TouchableOpacity>
+                                                        <Text style={styles.titleHeader}>Image</Text>
+                                                        <TouchableOpacity
+                                                                onPress={() => {
+                                                                        this.setState({
+                                                                                modalVisible: !this.state.modalVisible
+                                                                        });
+                                                                }} >
+                                                                <Icon name='check' size={30} color='#22D499' />
+                                                        </TouchableOpacity>
+                                                </View>
+                                                <FlatList
+                                                        data={this.state.data}
+                                                        extraData={this.state}
+                                                        numColumns={3}
+                                                        horizontal={false}
+                                                        renderItem={(item) => {
+
+                                                                return (
+                                                                        <TouchableOpacity >
+                                                                                {/* <Image source={{ uri: 'https://viknews.com/vi/wp-content/uploads/2019/04/Hot-girl-Tr%C3%A2m-Anh.jpg' }}
+                                                                                        style={{
+                                                                                                height: width / 3,
+                                                                                                width: width / 3,
+                                                                                                borderWidth: 1,
+                                                                                                borderColor: 'white'
+                                                                                        }} /> */}
+                                                                                <View>
+                                                                                        <Image source={{ uri: 'https://viknews.com/vi/wp-content/uploads/2019/04/Hot-girl-Tr%C3%A2m-Anh.jpg' }}
+                                                                                                style={{
+                                                                                                        height: width / 3,
+                                                                                                        width: width / 3,
+                                                                                                        borderWidth: 5,
+                                                                                                        borderColor: 'white'
+                                                                                                }} />
+                                                                                        <Icon name='checkcircle' size={30} color='#22D499' style={{
+                                                                                                position: 'absolute',
+                                                                                                right: 0
+                                                                                        }} />
+                                                                                </View>
+                                                                        </TouchableOpacity>
+                                                                );
+                                                        }}
+                                                />
+                                        </View>
+                                </Modal>
                         </View>
                 );
         }
@@ -85,5 +181,21 @@ const styles = StyleSheet.create({
                 width: 300,
                 borderBottomWidth: 1,
                 fontFamily: 'OpenSans-Regular',
+        },
+        containerModal: {
+                flex: 1
+        },
+        headerModal: {
+                height: 60,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                borderBottomWidth: 1,
+                borderBottomColor: 'rgba(0,0,0,0.3)'
+        },
+        titleHeader: {
+                fontFamily: 'UVN-Baisau-Regular',
+                color: 'black',
+                fontSize: 30,
         }
 });
